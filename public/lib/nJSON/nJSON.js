5ba3af608b55dbe51427b62a897c0a7ca50cee97
itsatony <i@itsatony.com>
2014-10-14 17:42:37 +0200

    frontend has a very simple filter now

--- public/lib/nJSON/nJSON.js
+++ public/lib/nJSON/nJSON.js
@@ -7,6 +7,7 @@ jQuery(document).ready(
 function initialize() {
 	var jsonData = JSON.parse(jQuery('#jsonData').text());
 	bindRefresh();
+	bindFilter();
 	render(jsonData);
 	return true;
 };
@@ -16,6 +17,17 @@ function render(jsonData) {
 	return true;
 };
 
+function bindFilter() {
+	jQuery('#filter').on(
+		'keyup',
+		function(e) {
+			if (e.which === 13) {
+				return filter( jQuery(this).val() );
+			}
+			return false;
+		}
+	);
+};
 function bindRefresh() {
 	jQuery('#refresh').on(
 		'click',
@@ -26,3 +38,37 @@ function bindRefresh() {
 		}
 	);
 };
+
+
+function filter(nodeName) {
+	var jsonData = JSON.parse(jQuery('#jsonData').text()); 
+	var renderData = [ 'filter->', jsonData ];
+	if (nodeName.length > 0) {
+		renderData = traverse(jsonData, nodeName, 'root');
+		if (renderData === false) {
+			jQuery('#chain').text('filter->');
+			return false;
+		}
+	}
+	jQuery('#chain').text(renderData[0] + '.');
+	jQuery('#filter').select();
+	return render(renderData[1]);
+};
+
+
+function traverse(data, name, chain) {
+	var subMatch = false;
+	for (var n in data) {
+		if (n === name) {
+			return [ chain, data[n] ];
+		} 
+		if (typeof data[n] === 'object') {
+			subMatch = traverse(data[n], name, chain + '.' +n );
+			if (subMatch !== false) {
+				return subMatch;
+			}
+		}
+	}
+	return false;
+};
+
